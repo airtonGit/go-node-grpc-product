ec35b1c129bbc343715263ba796be98e7655974d
Airton F. S. Oliveira <airton83@icloud.com>
2020-09-26 13:02:01 -0300

    Ajuste no path do arquivo gerado

---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"context"
+
+	pb "github.com/airtonGit/go-node-grpc-product/product"
+)
+
+type discountServer struct {
+}
+
+func (s *discountServer) Discount(ctx context.Context, req *pb.DicountRequest) (*pb.DiscountRe) {
+
+}
